0b363ce9f0024b52da60c68d6a099fdce6a34993
Lokesh Kothapalli <lokeshkothapalli77@gmail.com>
2026-08-17 22:58:24 +0530

    Add initial car simulation

---
+++ main.py
@@ -0,0 +1,9 @@
+#Braking and Speed Tracking
+Speed = 60
+#when break applied speed reduced by 20 so,
+Speed -=20
+print("Speed when brak applied = ",Speed)
+#when speed is increased by 10
+Speed +=10
+print("After increasing speed by 10 =",Speed)
+
